bb5d3dfe42f478f3b1316889a0e10bd30889650a
Hans Bakker <hansmbakker@hotmail.com>
2020-07-13 20:14:21 +0200

    Add XamlApplication project

---
+++ XamlApplicationProject/App.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace XamlApplicationProject
+{
+    public sealed partial class App : Microsoft.Toolkit.Win32.UI.XamlHost.XamlApplication
+    {
+        public App()
+        {
+            this.Initialize();
+        }
+    }
+}
